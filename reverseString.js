dc4e1dcc8218c7932ba28feaa4bf6e7856116065
LaTy49 <batyrbekovbektursun26.07.08@gmail.com>
2025-01-31 09:57:23 +0600

    Create reverseString.js

---
+++ reverseString.js
@@ -0,0 +1,11 @@
+function task3(ngp){
+    if (ngp > 0){
+        return "positive"
+    }
+    else if(ngp < 0){
+        return "negative"
+    }
+    else(ngp === 0){
+        return "zero"
+    }
+}
